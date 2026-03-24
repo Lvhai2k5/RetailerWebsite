40600b5b113774cd149b631c0be817ece6f00e42
Lvhai2k5 <lehai332k5@gmail.com>
2026-03-24 19:06:44 +0700

    chore: initial commit

---
+++ RetailStoreManagement/src/test/java/ute/fit/RetailStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
